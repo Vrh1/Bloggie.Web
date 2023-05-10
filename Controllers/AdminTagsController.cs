fbf80ca67181d80a5b2e994f035e938ae2941407
Vergara <jvv10.vergara@gmail.com>
2023-05-10 14:42:51 -0300

    Aula 03
    CRUD completo.

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using udemyBloggie.Web.Data;
+using udemyBloggie.Web.Models.Domain;
 using udemyBloggie.Web.Models.ViewModels;
 
 namespace udemyBloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -19,10 +29,106 @@ namespace udemyBloggie.Web.Controllers
             //var displayName = Request.Form["displayName"];
 
             // Model Binding:
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+            //var name = addTagRequest.Name;
+            //var displayName = addTagRequest.DisplayName;
 
-            return View("Add");
+
+            //Mapping AddTagRequest to Tagdomain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("List");
         }
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            // use dbContext to read the tags.
+            var tags = bloggieDbContext.Tags.ToList();
+
+
+            return View(tags);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            // 1st method
+            //var tag = bloggieDbContext.Tags.Find(id);
+            
+            // 2nd Method
+            var tag = bloggieDbContext.Tags.FirstOrDefault(x => x.Id == id);
+
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+
+                return View(editTagRequest);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            // Boa prática de verificação.
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName,
+            };
+
+            var existingTag = bloggieDbContext.Tags.Find(tag.Id);
+
+            if (existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                // Save changes.
+                bloggieDbContext.SaveChanges();
+
+                // Show sucess notification.
+                return RedirectToAction("List");
+            }
+
+            // Show error notification.
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(EditTagRequest editTagRequest)
+        {
+            var tag = bloggieDbContext.Tags.Find(editTagRequest.Id);
+
+            if (tag != null)
+            {
+                bloggieDbContext.Tags.Remove(tag);
+                bloggieDbContext.SaveChanges();
+
+                // Show a succes notification
+                return RedirectToAction("List");
+            }
+
+            // Show a failure notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # udemyBloggie.Web
-## ASP.NET MVC
+## ASP.NET MVC / Entity Framework / MySqlServer
 
 Aula 01 - Criação do projeto MVC,
 Criação do Models Domain.
@@ -15,3 +15,11 @@ View de Add criada,
 Adicionado no layout principal da aplicação um navbar do bootstrap para Admin e comandos do ASP para acessá-las,
 Formulário para Tag Admin.
 
+Aula 03 - Integração com o banco de dados,
+Criação de tags,
+Listagem de tags,
+Como ter código C# dentro de uma razor page (.cshtml)
+Delete de tags,
+Criação das view List Add e Edit,
+ViewModels como uma boa pratica.
+Sistema CRUD completo.
